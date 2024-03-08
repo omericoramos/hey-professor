fa27a60e83a95c6e68c7b4d680acd26fc74b9026
omericoramos <omericoramos@gmail.com>
2024-03-07 21:45:00 -0300

    feature/Hey-8-criar-pregunta: Adicinando rule ao teste de verificação de interrogação

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Closure;
+use App\Rules\EndWithQuestionMarkRule;
 use Illuminate\Http\RedirectResponse;
 
 class QuestionController extends Controller
@@ -15,11 +15,7 @@ class QuestionController extends Controller
                 'question' => [
                     'required',
                     'min:10',
-                    function (string $attribute, mixed $value, Closure $fail) {
-                        if ($value[strlen($value) - 1] != '?') {
-                            $fail('Are you sure that is a questions? It is missing to question mark in the end.');
-                        }
-                    },
+                    new EndWithQuestionMarkRule(),
                 ],
             ]
         );
