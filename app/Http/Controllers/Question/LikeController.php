2230f9117e5b4aea8569dc145e90e3619ebc308e
omericoramos <omericoramos@gmail.com>
2024-04-03 22:46:26 -0300

    feature/hey-15: refatorando o método de ação no model User para curtir uma pergunta

--- app/Http/Controllers/Question/LikeController.php
+++ app/Http/Controllers/Question/LikeController.php
@@ -4,19 +4,16 @@ namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
 use App\Models\Question;
-use App\Models\Vote;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
-        Vote::query()->create([
-            'user_id' => auth()->user()->id,
-            'question_id' => $question->id,
-            'like' => 1,
-            'unlike' => 0,
-        ]);
+        /** @var \App\Models\User $user * */
+        $user = Auth::user();
+        $user->like($question);
 
         return back();
     }
