a8ff417cd3fe2faa3c36695870f9e73018c12ad3
omericoramos <omericoramos@gmail.com>
2024-04-03 22:58:31 -0300

    feature/hey-15: adicionando test para inibir mais de um like do usuário na mesma questão

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -29,3 +29,22 @@ it('shuld be able to like a question', function () {
         'unlike' => 0,
     ]);
 });
+
+it('shuld not be able to like more than 1 time', function () {
+
+    // cria um usuario
+    $user = User::factory()->create();
+
+    // cria uma pergunta
+    $question = Question::factory()->create();
+
+    // Faz login com o usuario criado
+    actingAs($user);
+
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
+});
