0ae5682ec47eb3cb82f3063a54e531c8702517d4
omericoramos <felipealencar.0014@gmail.com>
2024-06-04 21:05:47 -0300

    feature/hey-19: deve retornar uma pagina de edição de pergunta

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -23,3 +23,20 @@ it('should be able to opne a question to edit', function () {
     get(route('question.edit', $question))->assertSuccessful();
 
 });
+
+// deve retornar uma pagina de edição de pergunta (edit.blade.php)
+it('should return a view', function () {
+
+    // primeiro eu crio o usuário
+    $user = User::factory()->create();
+
+    // depois eu crio a pergunta
+    $question = Question::factory()->for($user, 'createdBy')->create();
+
+    // e eu faço o login com o usuário
+    actingAs($user);
+
+    // então eu acesso a rota de edição da pergunta
+    get(route('question.edit', $question))->assertViewIs('question.edit');
+
+});
